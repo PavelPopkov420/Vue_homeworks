5e71c7f02362a0eef7d3eca5c5ed161413aa2d56
Pavel Popkov <goil2011@yandex.ru>
2024-06-20 15:51:15 +0300

    homework3

---
+++ homeworks/homework3/components.js
@@ -0,0 +1,124 @@
+Vue.component('change-Kitchen', {
+
+    template: `
+    
+<div>
+<h1 class="blogDetails__mid__content__right__header">!Кухня! Design sprints are great</h1>   
+<p class="blogDetails__mid__content__right__text">В своей статье от 1994-го года журнал «Before & After» отследил фразу «Lorem ipsum ...» до философского трактата Цицерона О пределах добра и зла, написанного в 45 году до нашей эры на латинском языке. 
+В оригинале текст выглядит так «Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit ...», и переводится как «Нет никого, кто любил бы свою боль, кто искал бы ее и хотел бы чтобы она была у него. Потому что это боль...»
+</p>
+    <div class="blogDetails__mid__content__right__quotes">
+    <h1 class="blogDetails__mid__content__right__quotes__header">“</h1>
+    <p class="blogDetails__mid__content__right__quotes__text">Какая-то умная и красивая цитата.</p>
+</div>
+    <h1 class="blogDetails__mid__content__right__header">Design sprints are great</h1>
+    <p class="blogDetails__mid__content__right__text">В оригинале текст выглядит так «Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit ...», и переводится как «Нет никого, кто любил бы свою боль, кто искал бы ее и хотел бы чтобы она была у него. Потому что это боль...»</p>
+        <ul class="blogDetails__mid__content__right__points">
+                    <ol class="blogDetails__mid__content__right__text"> <span
+                            class="blogDetails__mid__content__right__points__text">1.</span>С того времени этот, 
+                            похожий на латинский, текст стал стандартом в печатной промышленности для примеров шрифтов и текстов..</ol>
+                    <ol class="blogDetails__mid__content__right__text"><span
+                            class="blogDetails__mid__content__right__points__text">2.</span>С того времени этот, похожий на латинский,
+                             текст стал стандартом в печатной промышленности для примеров шрифтов и текстов.</ol>
+                    <ol class="blogDetails__mid__content__right__text"><span
+                            class="blogDetails__mid__content__right__points__text">3.</span>C того времени этот, похожий на латинский,
+                             текст стал стандартом в печатной промышленности для примеров шрифтов и текстов..</ol>
+                </ul>
+                
+                <img class="blogDetails__mid__content__right__img" src="img/blogDetaisImg2.png" alt="photo">
+                <p class="blogDetails__mid__content__right__text">В своей статье от 1994-го года журнал «Before & After» отследил фразу «Lorem ipsum ...» 
+                до философского трактата Цицерона О пределах добра и зла, написанного в 45 году до нашей эры на латинском языке. </p>
+    </div>
+    
+    `
+})
+Vue.component('change-Bedroom', {
+
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    template: `
+    <div>
+    <h1 class="blogDetails__mid__content__right__header">!Спальня! Design sprints are great</h1>
+    <p class="blogDetails__mid__content__right__text">В своей статье от 1994-го года журнал «Before & After» отследил фразу «Lorem ipsum ...» до философского трактата Цицерона О пределах добра и зла, написанного в 45 году до нашей эры на латинском языке. 
+    В оригинале текст выглядит так «Neque porro quisquam est qui dolorem 
+    ipsum quia dolor sit amet, consectetur, adipisci velit ...», и переводится к
+    ак «Нет никого, кто любил бы свою боль, кто искал бы ее и хотел бы чтобы она была у него. Потому что это боль...»
+    </p>
+        <div class="blogDetails__mid__content__right__quotes">
+        <h1 class="blogDetails__mid__content__right__quotes__header">“</h1>
+        <p class="blogDetails__mid__content__right__quotes__text">Какая-то умная и красивая цитата</p>
+    </div>
+    </div>
+    `
+})
+Vue.component('change-Building', {
+
+    template: `
+    <div>
+    <h1 class="blogDetails__mid__content__right__header">!Здание! Design sprints are great</h1>
+    <p class="blogDetails__mid__content__right__text">В своей статье от 1994-го года журнал «Before & After» отследил фразу «Lorem ipsum ...» до философского трактата Цицерона О пределах добра и зла, написанного в 45 году до нашей эры на латинском языке. 
+    В оригинале текст выглядит так «Neque porro quisquam est qui dolorem 
+    ipsum quia dolor sit amet, consectetur, adipisci velit ...», и переводится к
+    ак «Нет никого, кто любил бы свою боль, кто искал бы ее и хотел бы чтобы она была у него. Потому что это боль...»</p>
+        <ul class="blogDetails__mid__content__right__points">
+                    <ol class="blogDetails__mid__content__right__text"> <span
+                            class="blogDetails__mid__content__right__points__text">1.</span>С того времени этот, 
+                            похожий на латинский, текст стал стандартом в печатной промышленности для примеров шрифтов и текстов.</ol>
+                    <ol class="blogDetails__mid__content__right__text"><span
+                            class="blogDetails__mid__content__right__points__text">2.</span>С того времени этот, похожий на латинский,
+                             текст стал стандартом в печатной промышленности для примеров шрифтов и текстов.</ol>
+
+                </ul>
+    </div>
+    `
+})
+
+Vue.component('change-Architecture', {
+
+    template: `
+    <div>
+    <h1 class="blogDetails__mid__content__right__header">!Архитектура! Design sprints are great</h1>
+    <p class="blogDetails__mid__content__right__text">В своей статье от 1994-го года журнал «Before & After» отследил фразу «Lorem ipsum ...» до философского трактата Цицерона О пределах добра и зла, написанного в 45 году до нашей эры на латинском языке. 
+    В оригинале текст выглядит так «Neque porro quisquam est qui dolorem 
+    ipsum quia dolor sit amet, consectetur, adipisci velit ...», и переводится к
+    ак «Нет никого, кто любил бы свою боль, кто искал бы ее и хотел бы чтобы она была у него. Потому что это боль.</p>
+    </div>
+    `
+})
+Vue.component('change-Kitchen-Planning', {
+
+    template: `
+    <div>
+    <h1 class="blogDetails__mid__content__right__header">!Планировка! Design sprints are great</h1>
+    <p class="blogDetails__mid__content__right__text">Существует множество вариаций отрывков
+    Lorem Ipsum доступен, но большинство пострадало. Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem
+        vitae turpmaximus.posuere in.Вопреки распространенному мнению.Существует множество вариаций отрывков из
+        Lorem Ipsum имеется, но большинство пострадало.</p>
+        <ul class="blogDetails__mid__content__right__points">
+                    <ol class="blogDetails__mid__content__right__text"> <span
+                            class="blogDetails__mid__content__right__points__text">1.</span>С того времени этот, похожий на латинский,
+                             текст стал стандартом в печатной промышленности для примеров шрифтов и текстов.</ol>
+            
+                </ul>
+    </div>
+    `
+})
+Vue.component('change-none', {
+    template: `
+    <div>
+    <change-Kitchen>  
+    </change-Kitchen>
+    <change-Bedroom>  
+    </change-Bedroom>
+    <change-Building>  
+    </change-Building>
+    <change-Architecture>  
+    </change-Architecture>
+    <change-Kitchen-Planning>  
+    </change-Kitchen-Planning>
+    </div>
+    `
+})
